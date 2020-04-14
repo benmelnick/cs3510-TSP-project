bde28621b4edaf2ae353d3e893135471c063058b
benmelnick <bmelnick3@gatech.edu>
2020-04-14 18:07:28 -0400

    Reformat main file to use the annealing.py file. Includes parsing the command line arguments and sending them to the SimulatedAnneal class. Call the tsp_annealing() function which runs the algorithm and outputs the path and cost. Note that the path contains every node once. Will need to output the start node twice in txt file.

--- tsp-3510.py
+++ tsp-3510.py
@@ -1,78 +1,21 @@
-import os
 import sys
-import math
-from collections import defaultdict
-import time
+from annealing import *
 
-# check this out: https://en.wiipedia.org/wiki/Christofides_algorithm
-class tsp:
+def main():
+    # problem set up - parse command line params
+    input_data = sys.argv[1]
+    max_time = int(sys.argv[2])
+    # todo: parse output file command line arg
 
-    path = sys.argv[1]
-    file = open(path, 'r')
-    file = file.readlines()
-    coord = []
-    pd = set()
-    visited = set()
+    sa = SimulatedAnneal(input_data, max_time)
+    best_path, best_cost = sa.tsp_annealing()
 
-    def pairwiseDistance(self):
-        # start_time = time.time()
-        self.getCoordinate()
-        
-        for a in self.coord:
-            for b in self.coord:
-                if a[0] != b[0] and (a[0], b[0]) not in self.visited and (b[0], a[0]) not in self.visited:
-                	self.visited.add((a[0], b[0]))
-                	dis = round(math.sqrt((a[2] - b[2])**2 + (a[1] - b[1])**2))
-                	self.pd.add((a[0], b[0], dis))
-        # end_time = time.time()
-        print(self.pd)
-        
-        
+    print("---------best path---------")
+    print(best_path)
+    print("---------best cost---------")
+    print(best_cost)
 
-    def getCoordinate(self):
-        
-        for line in self.file:
-            temp = line.rstrip('\n').split()
-            self.coord.append((int(temp[0]), float(temp[1]), float(temp[2])))
-    
+    # todo: print results to output txt file
 
-    def size(self):
-        return len(self.file)
-
-    #1. Create a minimum spanning tree T of G.
-    class Graph:
-        
-        def __init__(self,vertices):
-	        self.V= vertices
-	        self.graph = []
-          
-   
-        
-        def addEdge(self,u,v,w):
-            self.graph.append([u,v,w]) 
-      
-       
-        def find(self, parent, i): 
-            if parent[i] == i: 
-                return i 
-            return self.find(parent, parent[i]) 
-      
-        
-        def union(self, parent, rank, x, y): 
-            
-        
-        def KruskalMST(self): 
-  
-	# 2. Let O be the set of vertices with odd degree in T.
-	# By the handshaking lemma, O has an even number of vertices.  
-
-	#3.Find a minimum-weight perfect matching M in the induced
-	#subgraph given by the vertices from O.
-
-	#4. Combine the edges of M and T to form a connected multigraph H in which each vertex has even degree.
-	#5. Form an Eulerian circuit in H.
-	#6. Make the circuit found in previous step into a Hamiltonian circuit by skipping repeated vertices (shortcutting).
-            
-            
-x = tsp()
-x.pairwiseDistance()
+if __name__ == "__main__":
+    main()
